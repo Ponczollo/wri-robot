7e41a1585a6f2523cc5e0c88500cf6b7016c68c2
Bartosz Mączka <maczkabartek@gmail.com>
2026-05-11 15:23:39 +0200

    task1 done

--- task1.py
+++ task1.py
@@ -1,57 +1,100 @@
-from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+#!/usr/bin/env python3
+
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1, INPUT_2
 from ev3dev2.sensor.lego import ColorSensor
-from ev3dev2.led import Leds
 from ev3dev2.button import Button
+from time import sleep
 
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
-# tank_drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), 0.5)
-
-BLACK = 1
-GREEN = 3
-RED = 5
-WHITE = 6
 
 left_sensor = ColorSensor(INPUT_1)
 right_sensor = ColorSensor(INPUT_2)
-
 button = Button()
 
-def turn_left():
-    tank_drive.on_for_seconds(SpeedPercent(-14), SpeedPercent(7), 0.2)
+BASE_SPEED = 12
+TURN_SPEED = 16
+TURN_SPEED_BACK = 20
 
-def turn_right():
-    tank_drive.on_for_seconds(SpeedPercent(7), SpeedPercent(-14), 0.2)
+BLACK_THRESHOLD = 25
+
+SLEEP_AFTER_MODE_CHANGE = 0.025
+LOOP_DELAY = 0.01
+
+
+def left_intensity():
+    return left_sensor.reflected_light_intensity
+
+
+def right_intensity():
+    return right_sensor.reflected_light_intensity
+
+
+def is_black(value):
+    return value < BLACK_THRESHOLD
+
+
+def drive(left_speed, right_speed):
+    tank_drive.on(SpeedPercent(left_speed), SpeedPercent(right_speed))
+
 
 def forward():
-    tank_drive.on_for_seconds(SpeedPercent(7), SpeedPercent(7), 0.2)
+    drive(BASE_SPEED, BASE_SPEED)
+
+
+def turn_left():
+    drive(-TURN_SPEED_BACK, TURN_SPEED)
+
+
+def turn_right():
+    drive(TURN_SPEED, -TURN_SPEED_BACK)
+
+
+def wait_for_press():
+    print("Press any button to start...")
+    while not button.any():
+        sleep(0.01)
+    while button.any():  # wait for release
+        sleep(0.01)
+
+
+def wait_for_stop():
+    if button.any():
+        while button.any():
+            sleep(0.01)
+        return True
+    return False
+
 
 while True:
-    
-    move_right = 0
-    move_left = 0
-    if left_sensor.color == BLACK and right_sensor.color == BLACK :
-        forward()
-    if left_sensor.color != BLACK and right_sensor.color == BLACK :
-        turn_right()
-    if left_sensor.color == BLACK and right_sensor.color != BLACK :
-        turn_left()
-    if left_sensor.color != BLACK and right_sensor.color != BLACK :
-        forward()
-    # print("LEFT: ",end='')
-    # if left_sensor.color != BLACK:
-    #     print("WHITE",end='')
-    #     move_left += 5
-    # elif left_sensor.color == BLACK:
-    #     print("BLACK",end='')
-    #     move_right += 10
-    #     move_left -= 5
-    # print("\nRIGHT: ",end='')
-    # if right_sensor.color != BLACK:
-    #     print("WHITE",end='')
-    #     move_right += 5
-    # elif right_sensor.color == BLACK:
-    #     print("BLACK",end='')
-    #     move_left += 10
-    #     move_right -= 5
-    # tank_drive.on_for_seconds(SpeedPercent(move_left), SpeedPercent(move_right), 0.3)
+    wait_for_press()
+
+    try:
+        while True:
+            if wait_for_stop():
+                print("Stopped.")
+                tank_drive.off()
+                break
+
+            left_value = left_intensity()
+            right_value = right_intensity()
+
+            left_black = is_black(left_value)
+            right_black = is_black(right_value)
+
+            if left_black and right_black:
+                forward()
+
+            elif not left_black and right_black:
+                turn_right()
+
+            elif left_black and not right_black:
+                turn_left()
+
+            else:
+                forward()
+
+            sleep(LOOP_DELAY)
+
+    finally:
+        tank_drive.off()
