9d4fec35dd007e6865f47eddeea46b1ffc6ce3c7
Bartosz Mączka <maczkabartek@gmail.com>
2026-05-18 16:23:35 +0200

    move arm up/down

---
+++ move_down.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import OUTPUT_C, MediumMotor, SpeedPercent
+
+lift_motor = MediumMotor(OUTPUT_C)
+
+LIFT_SPEED = 10
+LIFT_ROTATIONS = -0.25
+
+lift_motor.on_for_rotations(SpeedPercent(LIFT_SPEED), LIFT_ROTATIONS, block=True, brake=True)
